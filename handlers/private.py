7c266a7584705731ad2b38c3ac4b0c8c5b7e667e
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-04-05 13:16:33 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,11 +7,11 @@ from helpers.filters import other_filters2
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
+    await message.reply_sticker("CAACAgIAAxkBAAFAK-hgasAgRyeKz7NS3ZKg1ULKfkk9NQAC4QADUomRIznBNrZHX5TMHgQ")
     await message.reply_text(
         f"""**Hey, I'm {bn} 🎵
 
-I can play music in your group's voice call. Developed by [Jason](https://t.me/ImJanindu).
+I can play music in your group's voice call. Developed by [](https://t.me/DeshadeethThisarana).
 
 Add me to your group and play music freely!**
         """,
